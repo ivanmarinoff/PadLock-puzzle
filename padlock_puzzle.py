d335d70568196771eef520e88c8cc1d0b61f04ed
Ivan Marinov <107050101+ivanmarinoff@users.noreply.github.com>
2022-11-13 17:08:00 +0200

    Update padlock_puzzle.py

--- padlock_puzzle.py
+++ padlock_puzzle.py
@@ -1,13 +1,13 @@
-def checknum(number, checknumber, num_correct, num_correct_position):
-    lnumber = str(number).zfill(len(checknumber))
-    return num_correct == sum(lnumber[i] in checknumber for i in range(len(checknumber))) and \
-        num_correct_position == sum(lnumber[i] == checknumber[i] for i in range(len(checknumber)))
+def padlock(number, checknumber, num_correct, num_correct_position):
+    number = str(number).zfill(len(checknumber))
+    return num_correct == sum(number[i] in checknumber for i in range(len(checknumber))) and \
+        num_correct_position == sum(number[i] == checknumber[i] for i in range(len(checknumber)))
 
 
-for cnumber in range(1000):
-    if checknum(cnumber, "682", 1, 1) and \
-            checknum(cnumber, "614", 1, 0) and \
-            checknum(cnumber, "206", 2, 0) and \
-            checknum(cnumber, "738", 0, 0) and \
-            checknum(cnumber, "780", 1, 0):
-        print("Code is:" + str(cnumber).zfill(3))
+for current_number in range(1000):
+    if padlock(current_number, "682", 1, 1) and \
+            padlock(current_number, "614", 1, 0) and \
+            padlock(current_number, "206", 2, 0) and \
+            padlock(current_number, "738", 0, 0) and \
+            padlock(current_number, "780", 1, 0):
+        print("Code is:" + str(current_number).zfill(3))
