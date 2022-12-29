02a812ea5c9f472a90133766f129fc4b50faf4d0
Ivan Marinov <107050101+ivanmarinoff@users.noreply.github.com>
2022-12-29 11:45:25 +0200

    Update padlock_puzzle.py

--- padlock_puzzle.py
+++ padlock_puzzle.py
@@ -11,3 +11,10 @@ for current_number in range(1000):
             padlock(current_number, "738", 0, 0) and \
             padlock(current_number, "780", 1, 0):
         print("Code is:" + str(current_number).zfill(3))
+'''
+One of the numbers is correct and positioned in the correct place - 682. 
+Nothing is right - 614. 
+One of the numbers is correct but positioned in the wrong place - 206. 
+Two of the numbers are correct but positioned in the wrong places - 738. 
+One of the numbers is correct but misplaced - 780. 
+'''
